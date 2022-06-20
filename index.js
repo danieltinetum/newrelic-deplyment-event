726a4a69cf54f6dff3b025c4962570e990157e71
danieltinetum <daniel.trejo@inetum.com>
2022-06-20 17:26:45 -0500

    Dist Recreated

---
+++ index.js
@@ -0,0 +1,10 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+const axios = require('axios')
+
+const time = (new Date()).toTimeString();
+const account_id = core.getInput('account-id')
+
+
+console.log("Your acount id:", account_id)
+core.setOutput("time", time);
