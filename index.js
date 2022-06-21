19f658a3821cf821c66e3dc446d6107315de9103
danieltinetum <daniel.trejo@inetum.com>
2022-06-21 17:11:27 -0500

    Times

--- index.js
+++ index.js
@@ -10,12 +10,14 @@ try {
         const context = github.context;
         const payload = context.payload;
 
-        console.log( JSON.stringify(payload.head_commit, null, 2) )
+        //console.log( JSON.stringify(payload.head_commit, null, 2) );
 
-        const commited_at = payload.head_commit.timestamp
+        const deployed_at = new Date().getTime();
+        const commited_at = new Date(payload.head_commit.timestamp).getTime();
 
-        console.log('1.-',commited_at )
-        console.log('2.-', new Date(commited_at).getTime())
+        console.log(deployed_at)
+        console.log(commited_at)
+
         
         core.setOutput("time", 10000 );
 
